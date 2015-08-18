c1951267edf95875145c556532d4fea177affe21
elnapoli <elnapoli>
2015-08-18 16:01:04 -0300

    boton "cerrar sessión" operativo
    se cambio el boton del tipo "a" al tipo "linkButton" lo cual permite manejar los enventos en el behind code de vb

--- MonitoreoCurricular/Layout.Master.vb
+++ MonitoreoCurricular/Layout.Master.vb
@@ -3,6 +3,17 @@
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
+        Btn_Logout.Attributes.Add("data-placement", "bottom")
+        Btn_Logout.Attributes.Add("data-original-title", "Logout")
+        Btn_Logout.Attributes.Add("data-toggle", "tooltip")
+
+
+
     End Sub
 
+    Protected Sub Btn_Logout_Click(sender As Object, e As EventArgs)
+
+        Session.Abandon()
+        Response.Redirect("/Login.aspx", True)
+    End Sub
 End Class
